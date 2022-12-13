2f18f382e9863d1786cc5500eb04dd7e77e8ad6b
deploy <deploy@users.noreply.github.com>
2022-12-13 05:20:29 +0000

    Updated

---
+++ esm-dubois-framer@0.1.1/dist/index.js
@@ -0,0 +1,11 @@
+// src/index.tsx
+import * as DuboisStyle from "./DuboisStyle.js";
+import { DuboisButton } from "./DuboisButton.js";
+import { DuboisSplitButton } from "./DuboisSplitButton.js";
+import { DuboisIcon } from "./DuboisIcon.js";
+export {
+  DuboisButton,
+  DuboisIcon,
+  DuboisSplitButton,
+  DuboisStyle
+};
