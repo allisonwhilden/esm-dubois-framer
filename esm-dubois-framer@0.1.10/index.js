2bd60e174d092d2eccec24812cedbf72b6b69c16
deploy <deploy@users.noreply.github.com>
2023-03-01 21:59:37 +0000

    Updated

---
+++ esm-dubois-framer@0.1.10/index.js
@@ -0,0 +1,23 @@
+// src/index.tsx
+import * as DuboisStyle from "./DuboisStyle.js";
+import { DuboisButton } from "./DuboisButton.js";
+import { DuboisSelect } from "./DuboisSelect.js";
+import { DuboisSwitch } from "./DuboisSwitch.js";
+import { DuboisSplitButton } from "./DuboisSplitButton.js";
+import { DuboisIcon } from "./DuboisIcon.js";
+import { DuboisDropdownMenu } from "./DuboisDropdownMenu.js";
+import { DuboisSegmentedControl } from "./DuboisSegmentedControl.js";
+import { DuboisBreadcrumb } from "./DuboisBreadcrumb.js";
+import { DuboisTag } from "./DuboisTag.js";
+export {
+  DuboisBreadcrumb,
+  DuboisButton,
+  DuboisDropdownMenu,
+  DuboisIcon,
+  DuboisSegmentedControl,
+  DuboisSelect,
+  DuboisSplitButton,
+  DuboisStyle,
+  DuboisSwitch,
+  DuboisTag
+};
